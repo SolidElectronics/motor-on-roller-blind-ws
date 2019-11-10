03f2ae3cb939b7fa9c28e8e2d27aca0ae67a29a2
philsson <philsson@gmail.com>
2019-11-10 17:40:13 +0100

    Configuration class added
    Uses Json to store on SPIFFS
    Connects relevant parameters to WifiManager in connect function

---
+++ config.cpp
@@ -0,0 +1,204 @@
+#include "config.h"
+
+#include "Arduino.h"
+#include <SPI.h>
+#include "FS.h"
+
+namespace philsson {
+namespace config {
+
+bool ConfigManager::needSaving = false;
+
+void setNeedSaving()
+{
+    ConfigManager::needSaving = true;
+}
+
+ConfigManager::ConfigManager()
+: m_config()
+, m_jsonConfig()
+, m_configFile("/config.json")
+, m_configSaved()
+, m_wmName(
+  "name", "Bonjour name", m_config.name, sizeof(m_config.name))
+, m_wmMqttServer(
+  "server", "MQTT Server", m_config.mqttServer, sizeof(m_config.mqttServer))
+, m_wmMqttPort(
+  "port", "MQTT port", m_config.mqttPort, sizeof(m_config.mqttPort))
+, m_wmMqttUID(
+  "uid", "MQTT username", m_config.mqttUID, sizeof(m_config.mqttUID))
+, m_wmMqttPWD(
+  "pwd", "MQTT password", m_config.mqttPWD, sizeof(m_config.mqttPWD))
+, m_wmText(
+  "<p><b>Optional MQTT server parameters:</b></p>")
+, m_wmText2(
+  "<script>t = document.createElement('div'); \
+   t2 = document.createElement('input'); \
+   t2.setAttribute('type', 'checkbox'); \
+   t2.setAttribute('id', 'tmpcheck'); \
+   t2.setAttribute('style', 'width:10%'); \
+   t2.setAttribute('onclick', \"if(document.getElementById('Rotation').value == 'false') \
+     {document.getElementById('Rotation').value = 'true'} \
+     else {document.getElementById('Rotation').value = 'false'}\"); \
+   t3 = document.createElement('label');tn = document.createTextNode('Clockwise rotation'); \
+   t3.appendChild(t2); \
+   t3.appendChild(tn); \
+   t.appendChild(t3); \
+   document.getElementById('Rotation').style.display='none'; \
+   document.getElementById(\"Rotation\").parentNode.insertBefore(t, document.getElementById(\"Rotation\")); \
+   </script>")
+{}
+
+void ConfigManager::run()
+{
+    if (needSaving)
+    {
+        saveConfig();
+        needSaving = false;
+    }
+}
+
+bool ConfigManager::init()
+{
+    if (!SPIFFS.begin())
+    {
+        Serial.println("Failed to mount file system");
+        return false;
+    }
+    return true;
+}
+
+bool ConfigManager::saveConfig()
+{
+    File file = SPIFFS.open(m_configFile, "w");
+    if (!file)
+    {
+        Serial.println("Failed to open config file for writing");
+        return false;
+    }
+
+    StaticJsonDocument<512> doc;
+    doc["name"] = m_config.name;
+    doc["mqttServer"] = m_config.mqttServer;
+    doc["mqttPort"] = m_config.mqttPort;
+    doc["mqttUID"] = m_config.mqttUID;
+    doc["mqttPWD"] = m_config.mqttPWD;
+    doc["blindPos"] = m_config.blindPos;
+    doc["blindMaxPos"] = m_config.blindMaxPos;
+    doc["directionInverted"] = m_config.directionInverted;
+
+    // Write
+    if (!serializeJson(doc, file))
+    {
+        Serial.println("Failed to write config to file");
+        return false;
+    }
+
+    file.close();
+
+    printConfig();
+
+    Serial.println("Saved JSON to SPIFFS");
+
+    return true;
+}
+
+bool ConfigManager::loadConfig()
+{
+    bool result = true;
+
+    File file = SPIFFS.open(m_configFile, "r");
+    if (!file)
+    {
+        Serial.println("Failed to open config file");
+        result = false;
+    }
+
+    StaticJsonDocument<512> doc;
+    if (result)
+    {
+        size_t size = file.size();
+        if (size > 1024)
+        {
+            Serial.println("Config file size is too large");
+            result = false;
+        }
+        else
+        {
+            printConfig();
+        }
+    }
+
+    DeserializationError error = deserializeJson(doc, file);
+    if (error)
+    {
+        Serial.println(F("Failed to read file, using default configuration"));
+        result = false;
+    }
+
+    m_config.setname(doc["name"] | "BlindX");
+    m_config.setmqttServer(doc["mqttServer"] | "");
+    m_config.setmqttPort(doc["mqttPort"] | "1883");
+    m_config.setmqttUID(doc["mqttUID"] | "");
+    m_config.setmqttPWD(doc["mqttPWD"] | "");
+    m_config.blindPos = doc["blindPos"] | -1;
+    m_config.blindMaxPos = doc["blindMaxPos"] | -1;
+    m_config.directionInverted = doc["directionInverted"] | 0;
+
+    Serial.println("Configuration completed");
+
+    return result;
+}
+
+bool ConfigManager::printConfig()
+{
+    File file = SPIFFS.open(m_configFile, "r");
+    if (!file)
+    {
+        Serial.println("Failed to open config file");
+        return false;
+    }
+
+    Serial.println("This is the loaded configuration:");
+    // Extract each characters by one by one
+    while (file.available())
+    {   
+        char c = file.read();
+        Serial.print(c);
+        if (c == char(','))
+        {
+            Serial.println();
+        }
+    }
+    Serial.println();
+
+    // Close the file
+    file.close();
+    return true;
+}
+
+bool ConfigManager::reset()
+{
+    SPIFFS.format();
+    // TODO: may want to reset wifimanager here. Maybe in main
+    //wifim.resetSettings();
+}
+
+Config& ConfigManager::getConfig()
+{
+    return m_config;
+}
+
+void ConfigManager::connectConfigToWifiManager(WiFiManager &wifiManager)
+{
+    wifiManager.addParameter(&m_wmName);
+    wifiManager.addParameter(&m_wmMqttServer);
+    wifiManager.addParameter(&m_wmMqttPort);
+    wifiManager.addParameter(&m_wmMqttUID);
+    wifiManager.addParameter(&m_wmMqttPWD);
+    wifiManager.addParameter(&m_wmText);
+    wifiManager.addParameter(&m_wmText2);
+}
+
+} // namespace config
+} // namespace philsson
---
+++ config.h
@@ -0,0 +1,92 @@
+#pragma once
+
+#include <WiFiManager.h> // Not used to store but at runtime
+#include <ArduinoJson.h>
+
+
+namespace philsson {
+namespace config {
+
+namespace {
+//! Getter and Setter for char* using String
+#define GETSET(x) \
+  String get##x() \
+    { return String(x); } \
+  void set##x(String y) \
+    { y.toCharArray(x, sizeof(x)); }
+}
+
+void setNeedSaving();
+
+struct Config
+{
+    // All these settings are stored in json format on SPIFFS
+
+    // WifiManager Settings
+    // These need to be char arrays
+    char name[20];
+    char mqttServer[20];
+    char mqttPort[10];
+    char mqttUID[10];
+    char mqttPWD[10];
+
+    // Settings outside of WifiManager
+    long blindPos;
+    long blindMaxPos;
+    bool directionInverted;
+
+    GETSET(name);
+    GETSET(mqttServer);
+    GETSET(mqttPort);
+    GETSET(mqttUID);
+    GETSET(mqttPWD);
+};
+
+class ConfigManager
+{
+public:
+    ConfigManager();
+
+    bool init();
+
+    //! Do nescessary tasks. E.g save config if needed
+    void run();
+
+    bool saveConfig();
+
+    //! Load the config from SPIFFS
+    bool loadConfig();
+
+    bool printConfig();
+
+    bool reset();
+
+    Config& getConfig();
+
+    //! Returns True if changes were done since last save
+    bool configChanged();
+
+    void connectConfigToWifiManager(WiFiManager &wifiManager);
+
+    static bool needSaving;
+
+private:
+
+    Config m_config;
+    JsonVariant m_jsonConfig;
+    String m_configFile;
+
+    bool m_configSaved;
+
+    //Define customer parameters for WIFI Manager
+    WiFiManagerParameter m_wmName;
+    WiFiManagerParameter m_wmMqttServer;
+    WiFiManagerParameter m_wmMqttPort;
+    WiFiManagerParameter m_wmMqttUID;
+    WiFiManagerParameter m_wmMqttPWD;
+    WiFiManagerParameter m_wmText;
+    WiFiManagerParameter m_wmText2;
+};
+
+} // namespace config
+} // namespace philsson
